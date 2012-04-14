11be84990cdfad9c32104b16d7e42659f6ffb6c5
abhat@J9978R1-ABHAT.corp.zynga.com <abhat@J9978R1-ABHAT.corp.zynga.com>
2012-04-14 22:20:47 +0530

    Code using maps.

--- numbers/recycle.py
+++ numbers/recycle.py
@@ -1,18 +1,30 @@
 import pprint
 import itertools
 import sys
+import time
 
-def areSame(no0, no1):
-    ln = len(no0) 
+start = time.time()
+
+def helper(no, lowBnd, hgBnd):
+    ln = len(no)
     con = ln
     end = 1
-    while con:
-        if (no1[-end:] + no1[0:(ln - end)]) == no0:
-            return True 
+
+    mp = {long(no): 1}
+
+    while con > 1:
+        genNo = long(no[-end:] + no[0:(ln - end)])
+        if (genNo >= lowBnd) and (genNo < hgBnd) and (genNo not in mp):
+            mp[genNo] = 1
         con = con - 1
         end = end + 1
 
-    return False
+    count = 0
+    for comb in itertools.combinations(mp.keys(), 2):
+        count = count + 1    
+
+    return (count, mp)
+
 
 lines = sys.stdin.readlines()
 
@@ -22,33 +34,29 @@ case = 1
 for line in lines[1:]:
     line = line.strip()
     bounds = line.split()
-    dct = {}
-
+    seen = {}
 
     low = long(bounds[0])
     high = long(bounds[1]) + 1
 
+    count = 0
     for no in xrange(low, high):
-        lst = [i for i in str(no)]
+        strNo = str(no)
+        lst = [i for i in strNo]
         lst.sort()
         st = ''.join(lst)
 
-        if st in dct:
-            dct[st].append(str(no))
+        if st in seen:
+            if no not in seen[st]:
+                retCount, mp = helper(strNo, low, high)
+                count = retCount + count
+                seen[st].update(mp)
         else:
-            dct[st] = [str(no)]
+            retCount, mp = helper(strNo, low, high)
+            count = retCount + count
+            seen[st] = mp 
 
-    count = 0
-    for nos in dct.values():
-        if len(nos) > 1:
-            combs = itertools.combinations(nos, 2)
-            for pair in combs:
-                (no0, no1) = pair
-                if len(no0) == 2:
-                    count = count + 1
-                else:
-                    if areSame(no0, no1):
-                        count = count + 1
-                 
     print prntFormat.format(case, count) 
     case = case + 1
+
+print time.time() - start
